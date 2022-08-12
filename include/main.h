a8334fb8a5b600bc16d6425c3f45e81c576f041b
TheLegolas <jakob.l.weiss@gmx.de>
2022-08-12 10:41:30 +0200

    implements multiple filters to smoothen the data, fixes false-idles

--- include/main.h
+++ include/main.h
@@ -2,5 +2,5 @@ void tick();
 void eval_data();
 void update_display();
 void check_idle();
-float mean_time();
+float mean_period();
 void clear_time_buf();
--- src/main.cpp
+++ src/main.cpp
@@ -6,85 +6,78 @@
 #include <main.h>
 
 #define ENABLE_SERIAL true
-#define ENABLE_BUZZER false
 
 //=====> hardware conf
-const int buzzer_pin = 3;
 const int transistor_pin = 2;
 
-//=====> spinner conf
 const int breaks_per_rev = 3;
 // radius in mm
 const float radius = 40e-03;
 const float circumfence = 2 * radius * PI;
 
+//=====> statemachine conf
+#define RUNNING 0
+#define IDLING 1
+#define WAKING 2
+volatile uint8_t state = IDLING;
+
 //======> filter conf
-const int filter_min_delta_time = 10;
+// minimum delay between two interrupts
+const int filter_min_time = 10;
+// max deviation from calculated mean
+const float filter_max_dev = 0.4f;
 
 //=====> buffer conf
 const int buf_length = 10;
 volatile int buf_index = 0;
-unsigned long time_buf[buf_length];
-unsigned long last_trigger_time = 0;
+volatile unsigned long time_buf[buf_length];
+volatile unsigned long last_trigger_time = 0;
 
 //=====> idle conf
-const unsigned long idle_timeout = 800;
-volatile bool is_idle = false;
-volatile bool clear_buf = false;
+const unsigned long idle_min_time = 1600;
+bool is_idle = false;
 
 //=====> eval variables
-float period, frequency, peripherial_speed;
+float period, frequency, peripheral_speed;
 
 //=====> display conf
 const unsigned long display_update_interval = 250;
 unsigned long last_display_update = 0;
 DisplayHandler displayHandler = DisplayHandler();
 
-void tick() {
+void tick()
+{
   // calculate delta_time and set last_trigger_time
   unsigned long delta_time = millis() - last_trigger_time;
-  if (delta_time < filter_min_delta_time) {
-    #if ENABLE_SERIAL
-      Serial.println("Datapoint discarded.");
-    #endif
-    return;
-  }
   last_trigger_time = millis();
 
-  if (delta_time >= idle_timeout) {
-    #if ENABLE_SERIAL
-    Serial.println("Entering Idle mode.");
-    #endif
-    // if idle condition was not met before, set clear_buf flag
-    if (!is_idle)
-      clear_buf = true;
-    // idle condition is met, set flags accordingly
-    is_idle = true;
-  } else {
-    if (!is_idle) {
-      // idle condition wasn't met before, proceed normally
-      buf_index = (buf_index + 1) % buf_length;
-      time_buf[buf_index] = delta_time;
-#if ENABLE_BUZZER
-      tone(buzzer_pin, 440, 3);
-#endif
-    } else {
-      // was idle before, reset idle flag
-      is_idle = false;
-    }
+  if (delta_time < filter_min_time)
+    return;
+
+  switch (state)
+  {
+  case IDLING:
+    state = WAKING;
+    break;
+
+  case WAKING:
+    state = RUNNING;
+  case RUNNING:
+    buf_index = (buf_index + 1) % buf_length;
+    time_buf[buf_index] = delta_time;
+    break;
   }
 }
 
-void setup() {
-// pin setup
-#if ENABLE_BUZZER
-  pinMode(buzzer_pin, OUTPUT);
-#endif
+void setup()
+{
+  // pin setup
   pinMode(transistor_pin, INPUT);
 
 // serial setup
 #if ENABLE_SERIAL
   Serial.begin(115200);
+  Serial.println("Connected.");
 #endif
 
   // prep the buffer
@@ -94,62 +87,91 @@ void setup() {
   displayHandler.init();
 
   // attach interrupts
-  attachInterrupt(digitalPinToInterrupt(transistor_pin), tick, FALLING);
+  attachInterrupt(digitalPinToInterrupt(transistor_pin), tick, RISING);
 }
 
-void loop() {
+void loop()
+{
   check_idle();
   update_display();
 }
 
-void eval_data() {
-  period = mean_time() * 3;
+void eval_data()
+{
+  period = mean_period();
   frequency = 1000 / period;
-  peripherial_speed = frequency * circumfence;
+  peripheral_speed = frequency * circumfence;
 #if ENABLE_SERIAL
   Serial.print("frequency: ");
   Serial.print(frequency);
   Serial.print(" Hz\tperipheral speed: ");
-  Serial.print(peripherial_speed);
+  Serial.print(peripheral_speed);
   Serial.println(" m/s");
 #endif
 }
 
-float mean_time() {
+float mean_period()
+{
   unsigned int count = 0;
   unsigned long sum = 0;
 
   if (is_idle)
     return INFINITY;
 
-  for (int i = 0; i < buf_length; i++) {
-    if (time_buf[i] != __LONG_MAX__) {
+  for (int i = 0; i < buf_length; i++)
+  {
+    if (time_buf[i] != __LONG_MAX__)
+    {
+      count++;
+      sum += time_buf[i];
+    }
+  }
+
+  if (count == 0)
+    return INFINITY;
+
+  float aux_mean = float(sum) / count;
+
+  // filter entries with calculated aux_mean
+  count = 0;
+  sum = 0;
+  for (int i = 0; i < buf_length; i++)
+  {
+    if (time_buf[i] == __LONG_MAX__)
+      continue;
+
+    // only select entries that do not deviate more than the specified percentage from the aux_mean
+    if (abs(1-time_buf[i]/aux_mean) < filter_max_dev){
       count++;
       sum += time_buf[i];
     }
   }
 
   if (count != 0)
-    return float(sum) / count;
-  return INFINITY;
+    return float(sum * breaks_per_rev) / count;
+  else
+    return INFINITY;
 }
 
-void check_idle() {
-  if (millis()-last_trigger_time >= idle_timeout){
-    #if ENABLE_SERIAL
-    Serial.println("Enabling IDLE due to inactivity");
-    #endif
-    is_idle = true;
-    clear_buf = true;
-  }
-  if (clear_buf) {
-    clear_buf = false;
+void check_idle()
+{
+  if (state == IDLING)
+    return;
+
+  unsigned long current = millis();
+  if (current - last_trigger_time < idle_min_time || current == last_trigger_time)
+    return;
+
+  if (state == RUNNING)
     clear_time_buf();
-  }
+
+  state = IDLING;
 }
 
-void update_display() {
-  if (millis() - last_display_update >= display_update_interval) {
+void update_display()
+{
+  if (millis() - last_display_update >= display_update_interval)
+  {
     last_display_update = millis();
     eval_data();
     displayHandler.clearBuffer();
@@ -158,9 +180,11 @@ void update_display() {
   }
 }
 
-void clear_time_buf() {
+void clear_time_buf()
+{
   noInterrupts();
-  for (int i = 0; i < buf_length; i++) {
+  for (int i = 0; i < buf_length; i++)
+  {
     time_buf[i] = __LONG_MAX__;
   }
   interrupts();
